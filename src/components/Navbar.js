e88c6bea40d1992768937105572f567eaa4e9fa1
shahed <shahed.rahman88@gmail.com>
2022-06-02 19:50:46 +0600

    mui icon added, navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { Search } from '@material-ui/icons'
 import React from 'react'
 import styled from 'styled-components'
 
@@ -6,13 +7,42 @@ const Container = styled.div`
 `
 const Wrapper = styled.div`
     padding: 10px 20px;
+    display: flex;
+    justify-content: space-between
+`
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center
+`
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer
+`
+const SearchContainer = styled.div`
+    border: 1px solid lightgray
+`
+
+const Center = styled.div`
+    flex: 1
+`
+const Right = styled.div`
+    flex:1
 `
 
 const Navbar = () => {
   return (
     <Container>
         <Wrapper>
-            Navbar
+            <Left>
+                EN
+                <SearchContainer>
+                    <Search />
+                    input
+                </SearchContainer>
+            </Left>
+            <Center>Center</Center>
+            <Right>Right</Right>
         </Wrapper>
     </Container>
   )
